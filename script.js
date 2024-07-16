09764b3a2d7d55f015f89687db319f79de32c4aa
Bhavesh Wagh <bhaveshbm2711@gmail.com>
2024-07-16 12:22:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -28,3 +28,11 @@ class Cat extends Animal {
 window.Animal = Animal;
 window.Dog = Dog;
 window.Cat = Cat;
+
+const myCat = new Cat("Siamese");
+myCat.makeSound(); // Output: The Siamese makes a sound
+myCat.purr(); // Output: purr
+
+const myDog = new Dog("Golden Retriever");
+myDog.makeSound(); // Output: The Golden Retriever makes a sound
+myDog.bark(); // Output: woof
